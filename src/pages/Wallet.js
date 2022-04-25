6e1e3e97561e9743ba2fe298892cf36aa4740e60
Thiago Mota <th.araujomota@gmail.com>
2022-04-25 17:18:08 -0300

    Adiciona parcialmente requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,13 +5,112 @@ import Header from '../components/Header';
 import { actionRequestCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      expenseValue: '',
+      expenseDescription: '',
+      currency: '',
+      paymentMethod: '',
+      expenseCategory: '',
+    };
+  }
+
   componentDidMount() {
     const { actionCurrencies } = this.props;
     actionCurrencies();
   }
 
+  handleChange = ({ target: { name, value} }) => {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <Header />;
+    const { expenseValue, expenseDescription, currency,
+      paymentMethod, expenseCategory } = this.state;
+    const { currencies } = this.props;
+    const paymentMethods = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+    const expenseTags = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+
+    return (
+
+      <main>
+        <Header />
+        <section>
+          <label htmlFor="expenseValue">
+            Valor
+            <input
+              data-testid="value-input"
+              name="expenseValue"
+              value={ expenseValue }
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <label htmlFor="expenseDescription">
+            Descrição
+            <input
+              data-testid="description-input"
+              name="expenseDescription"
+              value={ expenseDescription }
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <label htmlFor="currency">
+            Moeda
+            <select
+              data-testid="currency-input"
+              name="currency"
+              value={ currency }
+              onChange={ this.handleChange }
+            >
+              { currencies.map((currencies) => (
+                <option key={ currencies }>
+                  { currencies }
+                </option>)) }
+
+            </select>
+          </label>
+
+          <label htmlFor="paymentMethod">
+            Método de pagamento
+            <select
+              data-testid="method-input"
+              name="paymentMethod"
+              value={ paymentMethod }
+              onChange={ this.handleChange }
+            >
+              { paymentMethods.map((methods) => (
+                <option key={ methods }>
+                  { methods }
+                </option>
+              )) }
+            </select>
+          </label>
+
+          <label htmlFor="expenseCategory">
+            Categoria
+            <select
+              data-testid="tag-input"
+              name="expenseCategory"
+              value={ expenseCategory }
+              onChange={ this.handleChange }
+            >
+              { expenseTags.map((tags) => (
+                <option key={ tags }>
+                  { tags }
+                </option>
+              )) }
+            </select>
+          </label>
+
+        </section>
+      </main>
+    );
   }
 }
 
@@ -20,8 +119,12 @@ const mapDispatchToProps = (dispatch) => ({
 
 });
 
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
 Wallet.propTypes = {
   actionCurrencies: propTypes.func.isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(Wallet);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
